d49d441bebf0962a2e131911c89f329b1b9476a9
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2022-08-25 21:17:13 +0530

    Time: 244 ms (24.34%), Space: 14.7 MB (54.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,23 +3,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        checkr = {""}
-        checkc = {""}
-        
-        n = len(mat)
-        m = len(mat[0])
-        
-        for i in range(n):
-            for j in range(m):
-                if mat[i][j] == 0:
-                    checkr.add(i)
-                    checkc.add(j)
-        
-        for i in range(n):
-            if i in checkr:
-                mat[i] = [0]*m
-                continue
-            
-            for j in range(m):
-                if j in checkc:
-                    mat[i][j] = 0
+        rows = len(mat)
+        cols = len(mat[0])
+
+        B0 = C0 = False
+        for ci in range(0, cols):
+            if mat[0][ci] == 0:
+                B0 = True
+        for ri in range(0, rows):
+            if mat[ri][0] == 0:
+                C0 = True
+        for ri in range(1, rows):
+            for ci in range(1, cols):
+                if mat[ri][ci] == 0:
+                    mat[0][ci] = 0
+                    mat[ri][0] = 0
+        for ri in range(1, rows):
+            for ci in range(1, cols):
+                if mat[0][ci] == 0 or mat[ri][0] == 0:
+                    mat[ri][ci] = 0
+                        
+        if B0: 
+            for ci in range(cols):
+                mat[0][ci] = 0
+        if C0: 
+            for ri in range(rows):
+                mat[ri][0] = 0
