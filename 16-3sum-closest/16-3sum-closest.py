801d82019e89366aedb96d61a562aca2b77f8092
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2022-08-30 22:53:01 +0530

    Time: 9275 ms (13.46%), Space: 14.2 MB (13.84%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,45 +1,19 @@
-class Solution(object):
-    def threeSumClosest(self, nums, target):
-	    # sort array in ascending order 
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        
-        # say initial sum is infinity
-        closestSum = float('inf')
-        
-        # iterate over the array
+        res=sum(nums[:3])
         for i in range(len(nums)-2):
-            
-            # put a left pointer 1 ahead of the current index
-            left = i+1
-            
-            # put a right pointer at the last index
-            right = len(nums)-1
-        
-            # iterate till left pointer is lesser than the right pointer
-            while left < right:
-                
-                # the current sum 
-                currSum = nums[i] + nums[left] + nums[right]
-                
-                # update the closest sum value if the absolute diff between target and current sum is lesser than the                    
-				# absolute diff between target and closest Sum
-                if abs(target - closestSum) > abs(target - currSum):
-                        closestSum = currSum  
-                
-                # move right pointer towards left (so the sum would be smaller - array is sorted) if the current sum is                  
-				# greater than the target
-                if currSum > target:
-                    right -=1
-                
-                # move left pointer towards right  (so the sum would be bigger - array is sorted) if the current sum is 
-                # lesser than the target
-                elif currSum < target:
-                   
-                    left +=1
-                
-                # if current sum is equal to the target than simply return the target
+            s=i+1
+            r=len(nums)-1
+            while s<r:
+                sumhere=nums[i]+nums[s]+nums[r]
+                if abs(sumhere-target)<abs(res-target):
+                    res=sumhere
+                if sumhere<target:
+                    s+=1
                 else:
-                    return target
+                    r-=1
+        return res
                 
-        # return closest sum - it would have the number that is closest to the target on the number line
-        return closestSum
+        
+        
