eabc7b2eeb145a3036b1e52502daf6b1a8f942c5
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2022-10-08 21:55:25 +0530

    Time: 288 ms (97.66%), Space: 14.3 MB (12.36%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,19 +1,35 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        res=sum(nums[:3])
-        for i in range(len(nums)-2):
-            s=i+1
-            r=len(nums)-1
-            while s<r:
-                sumhere=nums[i]+nums[s]+nums[r]
-                if abs(sumhere-target)<abs(res-target):
-                    res=sumhere
-                if sumhere<target:
-                    s+=1
+        return self.KSumClosest(nums, 3, target)
+
+    def KSumClosest(self, nums: List[int], k: int, target: int) -> int:
+        N = len(nums)
+        if N == k:
+            return sum(nums[:k])
+
+        # target too small
+        current = sum(nums[:k])
+        if current >= target:
+            return current
+
+        # target too big
+        current = sum(nums[-k:])
+        if current <= target:
+            return current
+        
+        if k == 1:
+            return min([(x, abs(target - x)) for x in nums], key = lambda x: x[1])[0]
+
+        closest = sum(nums[:k])
+        for i, x in enumerate(nums[:-k+1]):
+            if i>0 and x == nums[i-1]:
+                continue
+            current = self.KSumClosest(nums[i+1:], k-1, target - x) + x
+            if abs(target - current) < abs(target - closest):
+                if current == target:
+                    return target
                 else:
-                    r-=1
-        return res
-                
-        
-        
+                    closest = current
+
+        return closest
