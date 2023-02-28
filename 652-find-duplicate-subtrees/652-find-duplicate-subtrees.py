7da58f5e95cc94578bb848114a36f953c1ad9fb9
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-02-28 11:13:06 +0530

    Time: 61 ms (44.31%), Space: 22.8 MB (40.07%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        
+        res = []
+        
+        hmap = {}
+        
+        def recurse(node, path):
+            if node is None:
+                return '#'
+            
+            path += ','.join([str(node.val), recurse(node.left, path), recurse(node.right, path)])
+            
+            if path in hmap:
+                hmap[path] += 1
+                if hmap[path] == 2:
+                    res.append(node)
+            else:
+                hmap[path] = 1
+                
+            
+            return path
+        
+        recurse(root, '')
+        #print(hmap) I SUGGEST YOU PRINT THIS - TO UNDERSTAND WHAT IS HAPPENING.
+        return res
