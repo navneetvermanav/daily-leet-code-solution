584941458e9bccf76a2053d1a11bad41156583c1
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-03-09 10:26:03 +0530

    Time: 44 ms (95.26%), Space: 17.4 MB (52.82%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast is not None:
+            fast = fast.next
+            if fast is None:
+                return None
+            fast = fast.next
+            slow = slow.next
+            if slow == fast:
+                break
+        if fast is None:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
