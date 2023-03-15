100aafca85379b2a6495f849bab78cc2c20f8079
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-03-15 11:27:08 +0530

    Time: 33 ms (83.59%), Space: 13.8 MB (96.89%) - LeetHub

---
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        null_seen=False
+        
+        queue=collections.deque([root])
+        while queue:
+            if null_seen:
+                return not any(queue)
+            else:
+                for _ in range(len(queue)):
+                    node=queue.popleft()
+                    
+                    if not node:
+                        null_seen=True
+                        
+                    else:
+                        if null_seen:
+                            return False
+                        else:
+                            queue.append(node.left)
+                            queue.append(node.right)
+                        
+        return True   
