33914774d88898df13f2b5020e9f95dfc1d40784
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-04-11 18:28:48 +0530

    Time: 476 ms (45.44%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-UPDATE Salary 
-SET sex = (CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END) 
+UPDATE salary SET sex= case when sex='f' then 'm' when sex='m' then 'f' end;
