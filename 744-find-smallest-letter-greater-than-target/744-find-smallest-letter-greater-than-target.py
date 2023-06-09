6d66a7e7c900ae80319742aaca7261ac796f7073
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-06-09 23:18:06 +0530

    Time: 136 ms (44.01%), Space: 16.7 MB (21.82%) - LeetHub

---
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0] # If not found
