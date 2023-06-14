2896c80b032ed71f2649feb2e9280ee8e4b0d1ef
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-06-14 23:20:06 +0530

    Time: 493 ms (75.63%), Space: 21.9 MB (27.12%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,10 @@
+class Solution:                                
+    def equalPairs(self, grid: List[List[int]]) -> int:
+
+        tpse = Counter(zip(*grid))                  # <-- determine the transpose
+                                                    #     and hash the rows
+
+        grid = Counter(map(tuple,grid))             # <-- hash the rows of grid. (Note the tuple-map, so
+                                                    #     we can compare apples w/ apples in next step.)
+
+        return  sum(tpse[t]*grid[t] for t in tpse) 
