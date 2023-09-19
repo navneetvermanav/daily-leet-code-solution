30fa8441cd8a068cfea4c5dc74b26be24714ccc2
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-09-19 11:16:49 +0530

    Time: 544 ms (47.95%), Space: 30.9 MB (92.66%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,16 +1,11 @@
-
 class Solution:
-    '''
-    def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in  range (0,len(nums)):
-            for j in range (1,len(nums)):
-                if [i]==[j+1]:
-                    return j+1
-        '''
-class Solution:
-    def findDuplicate(self, arr: List[int]) -> int:
-        arr.sort()
-        for i in range(len(arr)-1):
-            if(arr[i]==arr[i+1]):
-                return arr[i]
+    def findDuplicate(self, nums):
+        slow, fast = nums[0], nums[0]
+        while True:
+            slow, fast = nums[slow], nums[nums[fast]]
+            if slow == fast: break
+           
+        slow = nums[0];
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+        return slow
