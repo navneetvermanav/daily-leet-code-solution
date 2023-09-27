c8ac95ada0385d2de645cac5cd8cf9aef6254c80
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-09-27 19:45:27 +0530

    Time: 44 ms (14.65%), Space: 16.2 MB (86.87%) - LeetHub

---
+++ 880-decoded-string-at-index/880-decoded-string-at-index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def decodeAtIndex(self, S, K):
+        lens, n = [0], len(S)
+        for c in S:
+            if c.isdigit():
+                lens.append(lens[-1]*int(c))
+            else:
+                lens.append(lens[-1] + 1)
+                
+        for i in range(n, 0, -1):
+            K %= lens[i]
+            if K == 0 and S[i-1].isalpha():
+                return S[i-1]
