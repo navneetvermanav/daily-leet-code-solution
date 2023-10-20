26d3af7f5831940c18ae7b4c8f3c2a27b843b553
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2023-10-20 18:56:23 +0530

    Time: 61 ms (67.10%), Space: 19.9 MB (64.31%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
@@ -1,23 +1,23 @@
 class NestedIterator:
-    def __init__(self, nestedList: [NestedInteger]):
-        def flatten(lst):
-            for i in lst:
-                if i.isInteger() == False:
-                    for j in flatten(i.getList()):
-                        yield j
-                else:
-                    yield i.getInteger()
-        
-        self.lst = (list(flatten(nestedList)))
-        self.len_lst = len(self.lst)
-        self.idx = 0
+    def __init__(self, nestedList: List[NestedInteger]):
+        def get(currList: List[NestedInteger]) -> Generator[int, None, None]:
+            """
+            Generator function to iterate through all nested NestedInteger objects and return its integer value.
+            :param currList: The current (nested) list of NestedIntegers to iterate through.
+            :yields:         The integer value of the current NestedInteger in the iteration.
+            """
+            for nestedInteger in currList:
+                if nestedInteger.isInteger():  # nestedInteger has a single integer value
+                    yield nestedInteger.getInteger()
+                else:                          # nestedInteger is a list of NestedIntegers
+                    yield from get(nestedInteger.getList())
+        self.generator = get(nestedList)               # Initialise the generator object with the given NestedInteger list
+        self.nextInteger = next(self.generator, None)  # Obtain the next (first) NestedInteger pre-emptively
     
     def next(self) -> int:
-        self.idx += 1
-        return self.lst[self.idx-1]
-
+        result = self.nextInteger                      # store the current NestedInteger integer value
+        self.nextInteger = next(self.generator, None)  # Obtain the next NestedInteger pre-emptively
+        return result
     
     def hasNext(self) -> bool:
-        if self.idx < self.len_lst:
-            return True
-        return False
+        return self.nextInteger is not None
