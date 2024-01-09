0c73711ec7d11829135ca85c7f287ac45f8c4e99
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2024-01-09 18:31:45 +0530

    Time: 118 ms (90.81%), Space: 24.5 MB (86.73%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -3,16 +3,9 @@ class Solution:
         
         if not root: 
             return 0
-        
-        # When value is less than low, everything on it's left doesn't matter, 
-        # so only return the sum from its right children
         if root.val<low: 
             return self.rangeSumBST(root.right,low,high)
-        
-        # Same thing for high.
         elif root.val>high: 
             return self.rangeSumBST(root.left,low,high)
-        
-        # The current value is in the range, so return the sum of its left, right and own value
         else:
             return root.val + self.rangeSumBST(root.right,low,high) + self.rangeSumBST(root.left,low,high)
