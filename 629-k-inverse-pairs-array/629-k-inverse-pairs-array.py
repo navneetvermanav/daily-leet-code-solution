ea99aef2be69ba9ad5ade5bc0ccd59146eb72e5a
Navneet Verma <75069034+navneetvermanav@users.noreply.github.com>
2024-01-27 19:45:20 +0530

    Time: 185 ms (89.29%), Space: 16.7 MB (87.14%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -0,0 +1,19 @@
+class Solution:
+                        # A very good description of the dp solution is at
+                        # https://leetcode.com/problems/k-inverse-pairs-array/solution/ 
+                        # The code below uses two 1D arrays--dp and tmp--instead if a 
+                        # 2D array. tmp replaces dp after each i-iteration.
+    def kInversePairs(self, n: int, k: int) -> int:
+        dp, mod = [1]+[0] * k, 1000000007
+        
+        for i in range(n):
+            tmp, sm = [], 0
+            for j in range(k + 1):
+                sm+= dp[j]
+                if j-i >= 1: sm-= dp[j-i-1]
+                sm%= mod
+                tmp.append(sm)
+            dp = tmp
+            #print(dp)       # <-- uncomment this line to get a sense of dp from the print output
+			                 #     try n = 6, k = 4; your answer should be 49.
+        return dp[k]
